d1e9acad0f4ea07148550305f0640935a65bceda
Md Moradul Islam <m.mdislam93@gmail.com>
2018-09-16 20:39:18 +0600

    Online bidding system V1

---
+++ view/home.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php
+		require 'view/common/head.php';
+		?>
+		
+	</head>
+	<body>
+
+		<div class="container-fluid">
+			<?php
+			require 'view/common/nav.php';
+			?>
+			<div class="carousel slide" id="carousel-578920">
+				<ol class="carousel-indicators">
+					<li data-slide-to="0" data-target="#carousel-578920" class="active"></li>
+					<li data-slide-to="1" data-target="#carousel-578920"></li>
+					<li data-slide-to="2" data-target="#carousel-578920"></li>
+					<li data-slide-to="3" data-target="#carousel-578920"></li>
+					<li data-slide-to="4" data-target="#carousel-578920"></li>
+					<li data-slide-to="5" data-target="#carousel-578920"></li>
+				</ol>
+				<div class="carousel-inner">
+					<div class="item active">
+						<img alt="Carousel Bootstrap First" src="productimages/auc.jpg" style="width: 100%;height: 500px">
+						<div class="carousel-caption">
+							<h4>
+								
+							</h4>
+							<p>
+								
+							</p>
+						</div>
+					</div>
+					<div class="item">
+						<img alt="Carousel Bootstrap Second" src="productimages/angularfront.jpg" style="width: 100%;height: 500px">
+						<div class="carousel-caption">
+							<h4>
+								
+							</h4>
+							<p>
+								
+							</p>
+						</div>
+					</div>
+					<div class="item">
+						<img alt="Carousel Bootstrap Third" src="productimages/Harley-Davidson-.jpg" style="width: 100%;height: 500px">
+						<div class="carousel-caption">
+							<h4>
+								
+							</h4>
+							<p>
+								
+							</p>
+						</div>
+					</div>
+					<div class="item">
+						<img alt="Carousel Bootstrap First" src="productimages/carsss.jpg" style="width: 100%;height: 500px">
+						<div class="carousel-caption">
+							<h4>
+								
+							</h4>
+							<p>
+								
+							</p>
+						</div>
+					</div>
+					<div class="item">
+						<img alt="Carousel Bootstrap First" src="productimages/burnt electric guitar.jpg" style="width: 100%;height: 500px">
+						<div class="carousel-caption">
+							<h4>
+								
+							</h4>
+							<p>
+								
+							</p>
+						</div>
+					</div>
+					<div class="item">
+						<img alt="Carousel Bootstrap First" src="productimages/Tesla black.jpg" style="width: 100%;height: 500px">
+						<div class="carousel-caption">
+							<h4>
+								
+							</h4>
+							<p>
+								
+							</p>
+						</div>
+					</div>
+				</div>
+				<a class="left carousel-control" href="#carousel-578920" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+				<a class="right carousel-control" href="#carousel-578920" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+			</div>
+			<hr />
+			<div class="row">
+				<?php
+				require 'view/common/categories.php';
+				?>
+				<div class="col-md-10">
+					<div class="row">
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail First" src="productimages/photo2.jpg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> Antiques </h3>
+									<p>
+										Dummy text here!Dummy text here!Dummy text here!
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=1">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail First" src="productimages/arts1.jpg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> Arts </h3>
+									<p>
+										Dummy text here!Dummy text here!Dummy text here!
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=2">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail Second" src="productimages/angularfront.jpg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> Cars </h3>
+									<p>
+										Dummy text here!Dummy text here!Dummy text here!
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=3">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail Third" src="productimages/BIKES 2.jpg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> Bikes </h3>
+									<p>
+										Dummy text here!Dummy text here!Dummy text here!
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=4">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail First" src="productimages/coins.jpg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> Coins </h3>
+									<p>
+										Dummy text here!Dummy text here!Dummy text here!
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=5">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail First" src="productimages/instruments.jpg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> Instruments </h3>
+									<p>
+										Dummy text here!Dummy text here!Dummy text here!
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=6">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail Second" src="productimages/slid.jpg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> Sports </h3>
+									<p>
+										Dummy text here!Dummy text here!Dummy text here!
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=7">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="thumbnail">
+								<img alt="Bootstrap Thumbnail Third" src="productimages/phone1.jpg" style="width: 100%; height: 220px">
+								<div class="caption">
+									<h3> Mobiles </h3>
+									<p>
+										Dummy text here!Dummy text here!Dummy text here!
+									</p>
+									<p>
+										<a class="btn btn-primary" style="width: 100%;" href="products.php?op=list&cat=8">Go!</a>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?php
+			require 'view/common/footer.php';
+			?>
+		</div>
+
+	</body>
+</html>
